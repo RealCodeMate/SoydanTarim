de558283a1b01e62cd23ee88be8ae64495290632
Ahmet Gürkan Yiğit <gurkanyigitgr@gmail.com>
2023-01-07 19:18:03 +0200

    add header, routing, react bootstrap

--- soydantarim/src/App.js
+++ soydantarim/src/App.js
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home';
+import Header from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Header/>
+      <Routes>
+        <Route element={<Home />} path="/" />
+      </Routes>
     </div>
   );
 }
---
+++ soydantarim/src/components/header.js
@@ -0,0 +1,52 @@
+import react from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+   
+    return (
+      <>
+        <Navbar key="lg" expand="lg" bg="light" sticky="top">
+          <Container fluid>
+            <Navbar.Brand as={NavLink} to="/">
+              <h1>
+                 Soydan Tarım
+              </h1>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+            <Navbar.Offcanvas
+              id="offcanvasNavbar-expand-lg"
+              aria-labelledby="offcanvasNavbarLabel-expand-lg"
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                  Menü
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link as={NavLink} to="/">
+                    <h5>Anasayfa</h5>
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/ürünler">
+                    <h5>Ürünler</h5>
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/hakkımızda">
+                    <h5>Hakkımızda</h5>
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/iletişim">
+                    <h5>İletişim</h5>
+                  </Nav.Link>
+                  
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
