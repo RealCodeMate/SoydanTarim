6731753fcf31e0feb9fff10c4feda6a7ee7a80f0
bayogluteoman <bayogluteoman@gmail.com>
2023-01-07 17:55:58 +0300

    initial commit

---
+++ soydantarim/src/pages/home.js
@@ -0,0 +1,10 @@
+import react from "react";
+import "../sass/pages/_home.scss";
+
+export default function Home(){
+    return(
+        <div>
+            <h1>Soydan Tarım</h1>
+        </div>
+    )
+}
