2ca1f165cb48bd58a306ce6d93708e17babd7ad7
bayogluteoman <bayogluteoman@gmail.com>
2023-01-11 23:55:20 +0300

    fix footer

--- soydantarim/src/components/footer.js
+++ soydantarim/src/components/footer.js
@@ -1,9 +1,40 @@
 import "../sass/components/_footer.scss";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <div className="footer">
-            <p>Copyright 2023 All Rights Reserved By 3rd Wave</p>
+            <div className="footer__container">
+                <div className="footer__container-info">
+                    <h3>Soydan Tarım</h3>
+                    <p>Risus venenatis, varius nisi eget, accumsan neque. Aenean ac tristique ante, eget temique ante, eget tempus felis quisque.</p>
+                </div>
+                <div className="footer__container-links">
+                    <h3>Site Haritası</h3>
+                    <ul>
+                        <div>
+                            <li>Anasayfa</li>
+                            <li>Ürünler</li>
+                        </div>
+                        <div>
+                            <li>Hakkımızda</li>
+                            <li>İletişim</li>
+                        </div>
+                    </ul>
+                </div>
+                <div className="footer__container-contact">
+                    <h3>İletişim</h3>
+                    <p>Yeni mahalle, Sanayi Cd. 3/c, 33330 Mezitli/Mersin</p>
+                    <p>(0324) 359 42 57</p>
+                    <div className="footer__container-contact-logos">
+                        <p>Twitter</p>
+                        <p>Instagram</p>
+                        <p>Facebook</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                <p>Copyright 2023 All Rights Reserved By 3rd Wave</p>
+            </div>
         </div>
     )
 }
