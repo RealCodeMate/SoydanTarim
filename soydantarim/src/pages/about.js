95806dda405815c1548036805d3673da323fc3bd
bayogluteoman <bayogluteoman@gmail.com>
2023-01-15 17:38:06 +0300

    add mission and vision section

--- soydantarim/src/pages/about.js
+++ soydantarim/src/pages/about.js
@@ -1,5 +1,7 @@
 import "../sass/pages/_about.scss";
 import image from '../assets/about.jpeg'
+import mission from '../assets/mission.jpg'
+import vision from '../assets/vision.jpg'
 
 export default function About() {
     return (
@@ -14,19 +16,36 @@ export default function About() {
                     <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
                 </div>
             </div>
-            <div className="about__bottom">
-                <div className="about__bottom--banner">
-                    <div className="about__bottom--banner--1">
+            <div className="about__middle">
+                <div className="about__middle--banner">
+                    <div className="about__middle--banner--1">
                         <h3>Güvenilir</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
-                    <div className="about__bottom--banner--2">
+                    <div className="about__middle--banner--2">
                         <h3>Profesyonel</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
-                    <div className="about__bottom--banner--3">
+                    <div className="about__middle--banner--3">
                         <h3>Uzman</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="about__bottom">
+                <div className="about__bottom--mission">
+                    <div className="mission">
+                        <h1>Misyonumuz</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <img src={mission} className="img__mission" />
+                </div>
+                <div className="about__bottom--vision">
+                    <img src={vision} className="img__vision" />
+                    <div className="vision">
+                        <h1>Vizyonumuz</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing eLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     </div>
                 </div>
             </div>
