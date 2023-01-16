fec91bc6a9f870de85c3cc261f4ed6f72b511a7a
bayogluteoman <bayogluteoman@gmail.com>
2023-01-17 01:38:41 +0300

    add gallery to site map

--- soydantarim/src/components/footer.js
+++ soydantarim/src/components/footer.js
@@ -14,6 +14,7 @@ export default function Footer() {
                         <div>
                             <li>Anasayfa</li>
                             <li>Ürünler</li>
+                            <li>Galeri</li>
                         </div>
                         <div>
                             <li>Hakkımızda</li>
