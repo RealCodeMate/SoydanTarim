641feb43088f184a6a67570b03d2f8d352eeb8f2
bayogluteoman <bayogluteoman@gmail.com>
2023-01-22 18:10:13 +0300

    fix background for sections

--- soydantarim/src/pages/home.js
+++ soydantarim/src/pages/home.js
@@ -40,79 +40,83 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="landingImages">
-        <img src={image} className="img1" />
-        <img src={image} className="img1" />
-        <img src={image} className="img1" />
-      </div>
-      <div className="landingInfo">
-        <div className="landingInfo__slogan">
-          <h3>
-            Üreticiler için Mersin'in
-            <br /> En Gözde Tarım ve Zirai İlaçları
-          </h3>
-        </div>
-        <div className="landingInfo__desc">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-            nisl consequat, auctor augue et, facilisis diam. Ut euismod placerat
-            lacus eu tempus. Sed quis ligula urna. Mauris facilisis massa
-            mauris, eget gravida dolor tempus vel. Aenean feugiat nunc a ex
-            imperdiet, eu egestas dolor ultriciesLorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Nullam at nisl consequat, auctor augue
-            et, facilisis diam. Ut euismod placerat lacus eu tempus. Sed quis
-            ligula urna. Mauris facilisis massa mauris, eget gravida dolor
-            tempus vel. Aenean feugiat nunc a ex imperdiet, eu egestas dolor
-            ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Nullam at nisl consequat, auctor augue et, facilisis diam. Ut
-            euismod placerat lacus eu tempus. Sed quis ligula urna.
-          </p>
-        </div>
-      </div>
 
-      <div className="landingChoose">
-        <div className="landingChoose__texts">
-          <h1>Neden Soydan Tarım?</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-            nisl consequat, auctor augue et, facilisis diam.
-          </p>
-          <div className="services">
-            <div className="services__heading">
-              <p>ICON</p>
-              <div>
-                <h3>Servis 1</h3>
-                <p>Lorem ipsum dolor sit</p>
-              </div>
-            </div>
-            <div className="services__heading">
-              <p>ICON</p>
-              <div>
-                <h3>Servis 2</h3>
-                <p>Lorem ipsum dolor sit</p>
-              </div>
-            </div>
-            <div className="services__heading">
-              <p>ICON</p>
-              <div>
-                <h3>Servis 3</h3>
-                <p>Lorem ipsum dolor sit</p>
-              </div>
-            </div>
-            <div className="services__heading">
-              <p>ICON</p>
-              <div>
-                <h3>Servis 4</h3>
-                <p>Lorem ipsum dolor sit</p>
-              </div>
-            </div>
+      <div className="section2">
+
+        <div className="landingImages">
+          <img src={image} className="img1" />
+          <img src={image} className="img1" />
+          <img src={image} className="img1" />
+        </div>
+        <div className="landingInfo">
+          <div className="landingInfo__slogan">
+            <h3>
+              Üreticiler için Mersin'in
+              <br /> En Gözde Tarım ve Zirai İlaçları
+            </h3>
           </div>
-          <div className="landingChoose__button__wrapper">
-            <a className="btn">Hakkımızda</a>
+          <div className="landingInfo__desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
+              nisl consequat, auctor augue et, facilisis diam. Ut euismod placerat
+              lacus eu tempus. Sed quis ligula urna. Mauris facilisis massa
+              mauris, eget gravida dolor tempus vel. Aenean feugiat nunc a ex
+              imperdiet, eu egestas dolor ultriciesLorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Nullam at nisl consequat, auctor augue
+              et, facilisis diam. Ut euismod placerat lacus eu tempus. Sed quis
+              ligula urna. Mauris facilisis massa mauris, eget gravida dolor
+              tempus vel. Aenean feugiat nunc a ex imperdiet, eu egestas dolor
+              ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Nullam at nisl consequat, auctor augue et, facilisis diam. Ut
+              euismod placerat lacus eu tempus. Sed quis ligula urna.
+            </p>
           </div>
         </div>
-        <div className="landingChoose__image">
-          <img src={image} className="img1" />
+
+        <div className="landingChoose">
+          <div className="landingChoose__texts">
+            <h1>Neden Soydan Tarım?</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
+              nisl consequat, auctor augue et, facilisis diam.
+            </p>
+            <div className="services">
+              <div className="services__heading">
+                <p>ICON</p>
+                <div>
+                  <h3>Servis 1</h3>
+                  <p>Lorem ipsum dolor sit</p>
+                </div>
+              </div>
+              <div className="services__heading">
+                <p>ICON</p>
+                <div>
+                  <h3>Servis 2</h3>
+                  <p>Lorem ipsum dolor sit</p>
+                </div>
+              </div>
+              <div className="services__heading">
+                <p>ICON</p>
+                <div>
+                  <h3>Servis 3</h3>
+                  <p>Lorem ipsum dolor sit</p>
+                </div>
+              </div>
+              <div className="services__heading">
+                <p>ICON</p>
+                <div>
+                  <h3>Servis 4</h3>
+                  <p>Lorem ipsum dolor sit</p>
+                </div>
+              </div>
+            </div>
+            <div className="landingChoose__button__wrapper">
+              <a className="btn">Hakkımızda</a>
+            </div>
+          </div>
+          <div className="landingChoose__image">
+            <img src={image} className="img1" />
+          </div>
         </div>
       </div>
       
