5e7b75ff76c114595e3ae34407217ef414e50adc
Gürkan <gurkanyigitgr@gmail.com>
2023-01-23 04:03:58 +0300

    change for products

--- soydantarim/src/pages/home.js
+++ soydantarim/src/pages/home.js
@@ -1,6 +1,6 @@
 import react from "react";
 import CommentCarousel from "../components/carousel";
-import image from "../assets/landingImg.jpg";
+import image from "../assets/mission.jpg";
 import wheat from "../assets/wheat.mp4";
 import "../sass/pages/_home.scss";
 import logo from "../assets/logo.png";
@@ -44,9 +44,7 @@ export default function Home() {
         </div>
       </div>
 
-
       <div className="section2">
-
         <div className="landingImages">
           <img src={image} className="img1" />
           <img src={image} className="img1" />
@@ -62,17 +60,18 @@ export default function Home() {
           <div className="landingInfo__desc">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
-              nisl consequat, auctor augue et, facilisis diam. Ut euismod placerat
-              lacus eu tempus. Sed quis ligula urna. Mauris facilisis massa
-              mauris, eget gravida dolor tempus vel. Aenean feugiat nunc a ex
-              imperdiet, eu egestas dolor ultriciesLorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Nullam at nisl consequat, auctor augue
-              et, facilisis diam. Ut euismod placerat lacus eu tempus. Sed quis
-              ligula urna. Mauris facilisis massa mauris, eget gravida dolor
-              tempus vel. Aenean feugiat nunc a ex imperdiet, eu egestas dolor
-              ultriciesLorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Nullam at nisl consequat, auctor augue et, facilisis diam. Ut
-              euismod placerat lacus eu tempus. Sed quis ligula urna.
+              nisl consequat, auctor augue et, facilisis diam. Ut euismod
+              placerat lacus eu tempus. Sed quis ligula urna. Mauris facilisis
+              massa mauris, eget gravida dolor tempus vel. Aenean feugiat nunc a
+              ex imperdiet, eu egestas dolor ultriciesLorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Nullam at nisl consequat,
+              auctor augue et, facilisis diam. Ut euismod placerat lacus eu
+              tempus. Sed quis ligula urna. Mauris facilisis massa mauris, eget
+              gravida dolor tempus vel. Aenean feugiat nunc a ex imperdiet, eu
+              egestas dolor ultriciesLorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Nullam at nisl consequat, auctor augue et,
+              facilisis diam. Ut euismod placerat lacus eu tempus. Sed quis
+              ligula urna.
             </p>
           </div>
         </div>
@@ -86,28 +85,60 @@ export default function Home() {
             </p>
             <div className="services">
               <div className="services__heading">
-                <img src={icon1} style={{ height: "50px", width: "50px", color: "white" }}></img>
+                <img
+                  src={icon1}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundColor: "#fff",
+                    borderRadius: "10px",
+                  }}
+                ></img>
                 <div className="services__texts">
                   <h4>Mükemmel Hizmet</h4>
                   <p>Lorem ipsum dolor sit</p>
                 </div>
               </div>
               <div className="services__heading">
-                <img src={icon4} style={{ height: "50px", width: "50px" }}></img>
+                <img
+                  src={icon4}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundColor: "#fff",
+                    borderRadius: "10px",
+                  }}
+                ></img>
                 <div className="services__texts">
                   <h4>Temiz Çalışma</h4>
                   <p>Lorem ipsum dolor sit</p>
                 </div>
               </div>
               <div className="services__heading">
-                <img src={icon3} style={{ height: "50px", width: "50px" }}></img>
+                <img
+                  src={icon3}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundColor: "#fff",
+                    borderRadius: "10px",
+                  }}
+                ></img>
                 <div className="services__texts">
                   <h4>Kalite ve Güvenilirlik</h4>
                   <p>Lorem ipsum dolor sit</p>
                 </div>
               </div>
               <div className="services__heading">
-                <img src={icon2} style={{ height: "50px", width: "50px" }}></img>
+                <img
+                  src={icon2}
+                  style={{
+                    height: "50px",
+                    width: "50px",
+                    backgroundColor: "#fff",
+                    borderRadius: "10px",
+                  }}
+                ></img>
                 <div className="services__texts">
                   <h4>Uzman Çiftçi</h4>
                   <p>Lorem ipsum dolor sit</p>
@@ -122,62 +153,183 @@ export default function Home() {
             <img src={image} className="img1" />
           </div>
         </div>
-      </div>
 
-      <div className="homeProducts">
-        <h1 className="homeProducts__header">Ürünlerimiz</h1>
-        <div className="homeProducts__elements">
-          <div className="hvr_img">
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p1} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Zirai İlaçlar</h5>
-            </Link>
-          </div>
-          <div className="hvr_img">
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p2} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Gübreler</h5>
-            </Link>
-          </div>
-          <div className="hvr_img">
+        <div className="homeProducts">
+          <h1 className="homeProducts__header">Ürünlerimiz</h1>
+          <div className="homeProducts__elements">
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Zirai <span>İlaçlar</span>
+                </h3>
+                <p class="card-hover__text">
+                  Akasit, Defoliant, Fumigant ve daha fazlası.
+                </p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
 
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p3} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Tohumlar</h5>
-            </Link>
-          </div>
-          <div className=" hvr_img">
+              <img src={p1}></img>
+            </div>
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Gübre<span>ler</span>
+                </h3>
+                <p class="card-hover__text">
+                  Ahır Gübresi, Kompostlar, Yeşil Gübreler ve daha fazlası.
+                </p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
 
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p4} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Fideler</h5>
-            </Link>
+              <img src={p2}></img>
+            </div>
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Tohum<span>lar</span>
+                </h3>
+                <p class="card-hover__text">
+                  Domates, Biber, Karpuz ve daha fazlası.
+                </p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+
+              <img src={p3}></img>
+            </div>
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Fide<span>ler</span>
+                </h3>
+                <p class="card-hover__text">
+                  Aşılı Fide, Marul Fidesi, Domates Fidesi ve daha fazlası
+                </p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+
+              <img src={p4}></img>
+            </div>
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Tarım<span> Aletleri</span>
+                </h3>
+                <p class="card-hover__text">
+                  Kültivatör, Saban, Fıskiye ve daha fazlası.
+                </p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+
+              <img src={p5}></img>
+            </div>
+            <div class="card-hover">
+              <div class="card-hover__content">
+                <h3 class="card-hover__title">
+                  Zeytin<span>yağı</span>
+                </h3>
+                <p class="card-hover__text">Organik Zeytinyağı</p>
+                <Link to="/ürünler/productA" className="card-hover__link">
+                  <span>Keşfet</span>
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                    />
+                  </svg>
+                </Link>
+              </div>
+
+              <img src={p6}></img>
+            </div>
           </div>
-          <div className=" hvr_img">
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p5} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Tarım Aletleri</h5>
-            </Link>
-          </div>
-          <div className=" hvr_img">
-            <Link to="/ürünler/productA" className="flex_products">
-              <img src={p6} style={{ height: "280px", width: "280px" }}></img>
-              <h5>Zeytinyağı</h5>
-            </Link>
+        </div>
+
+        <div className="partners__section">
+          <Partners />
+        </div>
+        <div className="comments">
+          <h1 className="comments__header">Müşteri Yorumları</h1>
+          <div className="comments__slider">
+            <CommentCarousel />
           </div>
         </div>
       </div>
-
-      <div className="partners">
-        <Partners />
-      </div>
-      <div className="comments">
-        <h1 className="comments__header">Müşteri Yorumları</h1>
-        <div className="comments__slider">
-          <CommentCarousel />
-        </div>
-      </div>
-
     </div>
   );
 }
