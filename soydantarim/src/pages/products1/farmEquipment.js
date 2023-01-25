3f43dd93658151b3c9145e4e1f188408d6bb65a4
bayogluteoman <bayogluteoman@gmail.com>
2023-01-25 17:27:24 +0300

    add farm equipments page

---
+++ soydantarim/src/pages/products1/farmEquipment.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ProductList from "../../components/pagination/ProductList";
+import Pagination from "../../components/pagination/Pagination";
+import farmEquipmentList from "../../utils/farmEquipmentList";
+import "../../sass/pages/_productsA.scss";
+
+export default function FarmEquipment() {
+    const [productData, setProductData] = useState(farmEquipmentList);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(8);
+
+
+    const lastPostIndex = currentPage * postsPerPage;
+    const firstPostIndex = lastPostIndex - postsPerPage;
+    const currentPosts = productData.slice(firstPostIndex, lastPostIndex);
+    return (
+        <div className='productsA'>
+            <h1>Tarım Aletleri</h1>
+            <ProductList productData={currentPosts} />
+            <Pagination
+                totalPosts={productData.length}
+                postsPerPage={postsPerPage}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+            />
+        </div>
+    )
+}
---
+++ soydantarim/src/utils/farmEquipmentList.js
@@ -0,0 +1,52 @@
+const farmEquipmentList = [
+    {
+        "id": "alet1",
+        "name": "Tulumba",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet2",
+        "name":"Çapa",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet3",
+        "name":"Hortum",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet4",
+        "name":"Budama Makası",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet5",
+        "name":"Aşı Makası",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet6",
+        "name":"Macun",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet7",
+        "name":"Kürek ve Kazma",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "alet8",
+        "name":"Tırmık",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+]
+
+export default farmEquipmentList; 
