fa0fe148da4e8f2672dba060b15e6ec8e54487b4
bayogluteoman <bayogluteoman@gmail.com>
2023-01-25 17:12:01 +0300

    add pesticides datas

---
+++ soydantarim/src/utils/pesticidesList.js
@@ -0,0 +1,16 @@
+const pesticidesList = [
+    {
+        "id": "hasere1",
+        "name": "Product 1",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+    {
+        "id": "hasere2",
+        "name": "Product 2",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": ""
+    },
+]
+
+export default pesticidesList; 
