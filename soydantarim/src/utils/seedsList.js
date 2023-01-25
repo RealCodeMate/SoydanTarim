270c180f967c016df15cbf0ed34495854210c888
bayogluteoman <bayogluteoman@gmail.com>
2023-01-25 16:54:49 +0300

    fix seeds data

--- soydantarim/src/utils/seedsList.js
+++ soydantarim/src/utils/seedsList.js
@@ -1,73 +1,13 @@
 const seedsList = [
     {
         "id": "tohum1",
-        "name": "Domates",
+        "name": "Sebze Tohumları",
         "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
         "info": ""
     },
     {
         "id": "tohum2",
-        "name": "Biber",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum3",
-        "name": "Fasülye",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum4",
-        "name": "Salatalık",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum5",
-        "name": "Marul",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum6",
-        "name": "Maydanoz",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum7",
-        "name": "Havuç",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum8",
-        "name": "Soğan",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum9",
-        "name": "Patlıcan",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum10",
-        "name": "Turp",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum11",
-        "name": "Kavun",
-        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
-        "info": ""
-    },
-    {
-        "id": "tohum12",
-        "name": "Karpuz",
+        "name": "Yeşillik Tohumları",
         "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
         "info": ""
     },
