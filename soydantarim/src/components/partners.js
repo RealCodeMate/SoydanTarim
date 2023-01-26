b18ccffbaf000e1ae304385c7070a7ae5bcf3d96
bayogluteoman <bayogluteoman@gmail.com>
2023-01-26 03:18:16 +0300

    fix partners logos and style

--- soydantarim/src/components/partners.js
+++ soydantarim/src/components/partners.js
@@ -1,27 +1,32 @@
 import '../sass/components/_partners.scss';
-import image from "../assets/hektasLogo.png";
+import hektasLogo from "../assets/partnersLogo/hektasLogo.png";
+import bayerLogo from "../assets/partnersLogo/bayerLogo.png";
+import dogatechLogo from "../assets/partnersLogo/dogatechLogo2.png";
+import akcLogo from "../assets/partnersLogo/akcLogo.png";
+import biolchimLogo from "../assets/partnersLogo/biolchimLogo.png";
+import octaviusLogo from "../assets/partnersLogo/octaviusLogo.png";
 export default function Partners() {
     return (
         <div className='partners'>
             <h1>Partner Firmalar</h1>
             <div className='partners__company'>
-                <div className='messi'>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={hektasLogo} className="companyImg" alt='hektas_Logo' />
                 </div>
-                <div>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={bayerLogo} className="companyImg" alt='bayer_Logo' />
                 </div>
-                <div>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={octaviusLogo} className="companyImg" alt='octavius_Logo' />
                 </div>
-                <div>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={akcLogo} className="companyImg" alt='akc_Logo' />
                 </div>
-                <div>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={biolchimLogo} className="companyImg" alt='biolchim_Logo' />
                 </div>
-                <div>
-                    <img src={image} className="companyImg" />
+                <div className='logo'>
+                    <img src={dogatechLogo} className="companyImg" alt='dogatech_Logo' />
                 </div>
             </div>
 
