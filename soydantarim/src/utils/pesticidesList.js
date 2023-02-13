73a5e550f10f9e4e5214264becfc0cc51cd0a802
bayogluteoman <bayogluteoman@gmail.com>
2023-02-13 11:55:33 +0300

    add pesicides datas

--- soydantarim/src/utils/pesticidesList.js
+++ soydantarim/src/utils/pesticidesList.js
@@ -1,16 +1,53 @@
 const pesticidesList = [
     {
         "id": "hasere1",
-        "name": "Product 1",
+        "name": "Alfa SC",
         "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
         "info": ""
     },
     {
         "id": "hasere2",
-        "name": "Product 2",
+        "name": "Chreysa Fibron Jel",
         "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
         "info": ""
     },
+    {
+        "id": "hasere3",
+        "name": "K-Otrine",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "SC 50"
+    },
+    {
+        "id": "hasere4",
+        "name": "Icon",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "10 CS"
+    },
+    {
+        "id": "hasere5",
+        "name": "Fatox",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "10 CS"
+    },
+    {
+        "id": "hasere6",
+        "name": "Maxforce ",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "Jel"
+    },
+    {
+        "id": "hasere7",
+        "name": "Radilon Paste",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "EC 50"
+    },
+    {
+        "id": "hasere8",
+        "name": "Tibtrap",
+        "image": "https://i.picsum.photos/id/70/200/200.jpg?hmac=hRU7tEHltyLUTf0bCrAWFXlPRXOBTsvCcvL-dIUG2CE",
+        "info": "10 CS"
+    },
+   
 ]
 
 export default pesticidesList; 
