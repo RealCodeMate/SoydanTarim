ef4caa7238976817d84a78d8a2abf38c4f5cf77a
Gürkan <gurkanyigitgr@gmail.com>
2023-02-22 02:59:31 +0300

    change for carousel

--- soydantarim/src/components/carousel.js
+++ soydantarim/src/components/carousel.js
@@ -1,89 +1,89 @@
-import Carousel from 'react-bootstrap/Carousel';
-import '../sass/components/_commentCarousel.scss';
-import abdullahTurer from '../assets/commentsUser/abdullahTurer.png'
-import cumaliOzdemir from '../assets/commentsUser/cumaliOzdemir.png'
-import fatihYalin from '../assets/commentsUser/fatihYalin.png'
-import nurKursun from '../assets/commentsUser/nurKursun.png'
+import Carousel from "react-bootstrap/Carousel";
+import "../sass/components/_commentCarousel.scss";
+import user from "../assets/commentsUser/user.png";
 
 export default function CommentCarousel() {
-    return (
-        <Carousel variant="dark" className="container-carousel">
-            <Carousel.Item interval={1500}>
-                <div className='d-block'>
-                    <div className='comment'>
-                        <div className='comment__container'>
-                            <div className='header'>
-                                <img src={abdullahTurer} className='profileIcon' />
-                                <div className='header__namedate'>
-                                    <h5>Abdullah Türer</h5>
-                                    <p>6 ay önce</p>
-                                </div>
-                            </div>
-                            <div className='text'>
-                                <h2>Bol çeşit, kaliteli ürünler ve güvenilir hizmet. Tavsiye ederim.</h2>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <Carousel variant="dark" className="container-carousel">
+      <Carousel.Item interval={3000}>
+        <div className="d-block">
+          <div className="comment">
+            <div className="comment__container">
+              <div className="header">
+                <img src={user} className="profileIcon" />
+                <div className="header__namedate">
+                  <h5>Abdullah Türer</h5>
+                  <p>6 ay önce</p>
                 </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={1500}>
-                <div className='d-block'>
-                    <div className='comment'>
-                        <div className='comment__container'>
-                            <div className='header'>
-                                <img src={cumaliOzdemir} className='profileIcon' />
-                                <div className='header__namedate'>
-                                    <h5>Cumali Ozdemir</h5>
-                                    <p>2 yıl önce</p>
-                                </div>
-                            </div>
-                            <div className='text'>
-                                <h2>Çok samimi bir ortam.</h2>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+              <div className="text">
+                <h2>
+                  Bol çeşit, kaliteli ürünler ve güvenilir hizmet. Tavsiye
+                  ederim.
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <div className="d-block">
+          <div className="comment">
+            <div className="comment__container">
+              <div className="header">
+                <img src={user} className="profileIcon" />
+                <div className="header__namedate">
+                  <h5>Cumali Ozdemir</h5>
+                  <p>2 yıl önce</p>
                 </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={1500}>
-                <div className='d-block'>
-                    <div className='comment'>
-                        <div className='comment__container'>
-                            <div className='header'>
-                                <img src={nurKursun} className='profileIcon' />
-                                <div className='header__namedate'>
-                                    <h5>Nur Kurşun</h5>
-                                    <p>1 yıl önce</p>
-                                </div>
-                            </div>
-                            <div className='text'>
-                                <h2>İletişim iyi bir satıcı</h2>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+              <div className="text">
+                <h2>Çok samimi bir ortam.</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <div className="d-block">
+          <div className="comment">
+            <div className="comment__container">
+              <div className="header">
+                <img src={user} className="profileIcon" />
+                <div className="header__namedate">
+                  <h5>Nur Kurşun</h5>
+                  <p>1 yıl önce</p>
                 </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={1500}>
-                <div className='d-block'>
-                    <div className='comment'>
-                        <div className='comment__container'>
-                            <div className='header'>
-                                <img src={fatihYalin} className='profileIcon' />
-                                <div className='header__namedate'>
-                                    <h5>Fatih Yalın</h5>
-                                    <p>3 yıl önce</p>
-                                </div>
-                            </div>
-                            <div className='text'>
-                                <h2>Tarım onların işi.</h2>
-                            </div>
-                        </div>
-                    </div>
+              </div>
+              <div className="text">
+                <h2>İletişim iyi bir satıcı</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <div className="d-block">
+          <div className="comment">
+            <div className="comment__container">
+              <div className="header">
+                <img src={user} className="profileIcon" />
+                <div className="header__namedate">
+                  <h5>Fatih Yalın</h5>
+                  <p>3 yıl önce</p>
                 </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
-};
+              </div>
+              <div className="text">
+                <h2>Tarım onların işi.</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
--- soydantarim/src/components/landingCarousel.js
+++ soydantarim/src/components/landingCarousel.js
@@ -1,29 +1,25 @@
-import Carousel from 'react-bootstrap/Carousel';
-import '../sass/components/_landingCarousel.scss';
+import Carousel from "react-bootstrap/Carousel";
+import "../sass/components/_landingCarousel.scss";
 
 export default function LandingCarousel() {
-    return (
-        <Carousel variant="dark" className="container-carousel2" fade>
-            <Carousel.Item interval={2500}>
-                <div className='d-block1'>
-                </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2500}>
-                <div className='d-block2'>
-                </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2500}>
-                <div className='d-block3'>
-                </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2500}>
-                <div className='d-block4'>
-                </div>
-                <Carousel.Caption className="carosuel-content"></Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
-    );
-};
+  return (
+    <Carousel variant="white" className="container-carousel2" fade>
+      <Carousel.Item interval={2500}>
+        <div className="d-block1"></div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2500}>
+        <div className="d-block2"></div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2500}>
+        <div className="d-block3"></div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2500}>
+        <div className="d-block4"></div>
+        <Carousel.Caption className="carosuel-content"></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
