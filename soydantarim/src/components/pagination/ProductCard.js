cd1eef79fc07bdf94d323afb8d6fd803505db381
bayogluteoman <bayogluteoman@gmail.com>
2023-03-30 10:49:39 +0300

    fix bugs

--- soydantarim/src/components/pagination/ProductCard.js
+++ soydantarim/src/components/pagination/ProductCard.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 import "../../sass/components/pagination/_productCard.scss"
+import messi from "../../assets/soydanTarimLogo.png";
 
 const ProductCard = ({ image, name, info }) => {
     return (
         <div className='cardProduct'>
             <div className='cardProduct_image'>
-                <img src={image} alt={name} />
+                {image != '' ?
+                    <img src={image} alt={name} />
+                    :
+                    <img src={messi} alt={name} style={{  objectFit:'contain' }} />
+                }
+
             </div>
             <div className='cardProduct_info'>
                 <h2>{name}</h2>
