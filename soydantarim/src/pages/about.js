75932512ba4a99727a0709202b16b9083df14fae
Gürkan <gurkanyigitgr@gmail.com>
2023-04-01 00:53:15 +0300

    fix import tags

--- soydantarim/src/pages/about.js
+++ soydantarim/src/pages/about.js
@@ -1,7 +1,4 @@
 import "../sass/pages/_about.scss";
-import image from "../assets/backgrounds/about.jpeg";
-import mission from "../assets/backgrounds/mission.jpg";
-import vision from "../assets/backgrounds/vision.jpg";
 import GoToTop from "../utils/GoToTop";
 
 export default function About() {
